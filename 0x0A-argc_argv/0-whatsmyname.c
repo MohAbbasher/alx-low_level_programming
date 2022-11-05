03287fc9db8673f69b11eeb800701e6e1e135f3a
Mohammed Abbasher <moh.abbasher@gmail.com>
2022-11-05 07:51:23 +0200

    file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+/**
+ * main- main function
+ * 
+ * @argc: args count
+ * @argv: args array
+ *
+ * Return: always 0
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
+
