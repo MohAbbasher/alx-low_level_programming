a7f6702771f896f2b2d29aab0d6479622c69e98b
Mohammed Abbasher <moh.abbasher@gmail.com>
2022-11-09 08:24:01 +0200

    1- argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- number of aarguments
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+
+return (0);
+}
