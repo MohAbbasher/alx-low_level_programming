d7db0ddecad785a69c46dff0984c16f570b93ebc
Mohammed Abbasher <moh.abbasher@gmail.com>
2022-11-15 15:42:44 +0200

    10

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initilize dog struct
+ *
+ * @d: dog struct
+ * @name: name
+ * age: age
+ * owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
