3d5e1796f9df7b8afe058368fc9e903c2267fbfa
Mohammed Abbasher <moh.abbasher@gmail.com>
2022-11-16 20:44:11 +0200

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stddef.h>
+/**
+ * int_index - return first accurance of element
+ *
+ * @array: array input
+ * @size: array size
+ * @cmp: comarison function
+ *
+ * Return: index of element or -1
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+
+		}
+
+		return (-1);
+	}
+}
