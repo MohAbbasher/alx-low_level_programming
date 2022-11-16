0fb9e72110784a3f7f60b9e535e28e13327eb987
Mohammed Abbasher <moh.abbasher@gmail.com>
2022-11-16 05:38:22 +0200

    11

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,13 +5,13 @@
  *
  * @d: dog struct
  * @name: name
- * age: age
- * owner: owner
+ * @age: age
+ * @owner: owner
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d)
+	if (d)
 	{
 		(*d).name = name;
 		(*d).age = age;
